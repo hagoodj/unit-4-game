178d1937e9fa5349c0ab3edba52cc552b6cdd4a5
jordan hagood <jordan.hagood@yahoo.com>
2019-09-04 16:06:18 -0700

    created reset.css, linked index.html to reset.css, created start button on game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,4 +10,19 @@ $(document).ready(function() {
     var value2;
     var value4;
 
+    $("#button-start").on("click", function() {
+
+        computerNumber = Math.floor(Math.random()*200) + 1;
+        totalScore = 0;
+        $("#target-score").html("Target Score: " + computerNumber); 
+        $("#your-score").html("Your Score: " + totalScore); 
+        $("#wins").html("Wins: " + wins); 
+        $("#losses").html("Losses: " + losses); 
+        value1 = Math.floor(Math.random()*20) + 1;
+        value2 = Math.floor(Math.random()*20) + 1;
+        value3 = Math.floor(Math.random()*20) + 1;
+        value4 = Math.floor(Math.random()*20) + 1;
+
+    })
+
 }
