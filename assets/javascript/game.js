92d2edda9c473cc1d465ebc0335d33f43d037e8f
jordan hagood <jordan.hagood@yahoo.com>
2019-09-04 15:15:49 -0700

    index.html contents and game.js created variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    var computerNumber = "";
+    var totalScore = 0;
+    var wins = 0;
+    var losses = 0;
+
+    var value1;
+    var value2;
+    var value2;
+    var value4;
+
+}
